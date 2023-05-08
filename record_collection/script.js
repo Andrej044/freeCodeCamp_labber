42691d3f9a07ddbd1ed7307476440c7ca9f6fbd6
Andrii Lishchyna <andrej044@i.ua>
2023-05-08 17:32:51 -0400

    add issue but test is not complete

--- record_collection/script.js
+++ record_collection/script.js
@@ -21,7 +21,28 @@ const recordCollection = {
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
+
+  if(prop === "tracks" && value !== "") {
+   
+    Array.isArray(records[id][prop]) ? records[id][prop] : records[id][prop] = [];  
+    let arr = records[id][prop];
+   arr.push(value);
+   return records[id][prop] = arr;
+  };
+  
+  if(value === "") delete records[id][prop];
+  else records[id][prop] = value;
+  
   return records;
 }
 
 updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+// updateRecords(recordCollection, 5439, 'track', '');
+// updateRecords(recordCollection, 1245, "albumTitle", "Riptide")
+// updateRecords(recordCollection, 2548, "tracks", "")
+// updateRecords(recordCollection, 2468, "tracks", "Free")
+// updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
+// updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
+
+
+console.log(recordCollection)
